a02d6fb47ac4dcccc14280bc004f80cf08eac2d8
Eugene Kalenik <e.kalenik@itransition.com>
2018-12-04 16:40:13 +0300

    Updates

---
+++ precache-manifest.9f1678f815d6d37b52ac8e8ed8530efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f76b658f94f874de8802",
+    "url": "/i6_news_react/static/js/runtime~main.f76b658f.js"
+  },
+  {
+    "revision": "036e5cf884b493e0ba54",
+    "url": "/i6_news_react/static/js/main.036e5cf8.chunk.js"
+  },
+  {
+    "revision": "03783d836442911ca812",
+    "url": "/i6_news_react/static/js/1.03783d83.chunk.js"
+  },
+  {
+    "revision": "036e5cf884b493e0ba54",
+    "url": "/i6_news_react/static/css/main.a7a503b2.chunk.css"
+  },
+  {
+    "revision": "5582a871a5fa7f5fdc2b0315c24ce208",
+    "url": "/i6_news_react/index.html"
+  }
+];
