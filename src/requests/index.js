bb774f0d28552916a8933e055c7f631f317c7f7f
Eugene Kalenik <20687992+eugenekalenik@users.noreply.github.com>
2018-12-09 23:56:12 +0300

    Update index.js

--- src/requests/index.js
+++ src/requests/index.js
@@ -2,7 +2,7 @@ import { BASE_URL, API_KEY } from '../constants';
 
 
 export const getNews = async (topic) => {
-  const NEWS_URL = BASE_URL + topic + '&apiKey=' + API_KEY;
+  const NEWS_URL = `${BASE_URL}${topic}&apiKey=${API_KEY}`;
 
   return await fetch(NEWS_URL)
     .then(response => response.json());
